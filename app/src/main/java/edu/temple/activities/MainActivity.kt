1623829763a08bc0bee735e4efa0e621c2b69a34
aidanb3765 <aidan.blaney@temple.edu>
2023-10-03 15:16:48 -0400

    Class activity work so far
    Creating an implementation for the onClickListener as well as working on the lambda

--- app/src/main/java/edu/temple/activities/MainActivity.kt
+++ app/src/main/java/edu/temple/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,7 +24,10 @@ class MainActivity : AppCompatActivity() {
         with (findViewById(R.id.textSizeSelectorRecyclerView) as RecyclerView) {
 
             // TODO Step2: Implement lambda body to launch new activity and pass value
-            adapter = TextSizeAdapter(textSizes){
+            adapter = TextSizeAdapter(textSizes) {
+                val launchIntent = Intent(this@MainActivity, DisplayActivity::class.java)
+                startActivity(launchIntent)
+                textSizes.contentToString() = it.toString()
 
             }
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -36,12 +40,12 @@ class MainActivity : AppCompatActivity() {
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter (private val textSizes: Array<Int>, callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
+class TextSizeAdapter (private val textSizes: Array<Int>, private val callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
 
     // TODO Step 1: Complete onClickListener to return selected number
     inner class TextSizeViewHolder(val textView: TextView) : RecyclerView.ViewHolder (textView) {
         init {
-            textView.setOnClickListener {  }
+            textView.setOnClickListener { callback(textSizes[adapterPosition]) }
         }
     }
 
